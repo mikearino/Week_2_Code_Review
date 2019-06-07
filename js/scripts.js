aa648fa8660c91f16ed8d38071a33a9063dee233
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-07 10:18:59 -0700

    Add functionality to form, Jquery recieves inputs from form to make variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   console.log("Jquery is working OOOOOHHHHHH!");
+  $("#submitbutton").click(function(event){
+    event.preventDefault();
+    var music = parseInt($("select#music").val());
+    var animal = parseInt($("select#animal").val());
+    var activity = parseInt($("select#activity").val());
+    var lost = parseInt($("select#lost").val());
+    var phone = parseInt($("select#phone").val());
+    console.log(music, animal, activity, lost, phone);
+
+    
+  });
 });
